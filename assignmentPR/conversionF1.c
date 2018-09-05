bd0a986ef508ff36d110ad8b4e7d636feaf8e6df
tanman987 <massahos@gmail.com>
2018-09-04 17:21:37 -0700

    Added character to Int method, make a makefile, and moved some things around

--- assignmentPR/conversionF1.c
+++ assignmentPR/conversionF1.c
@@ -4,30 +4,53 @@
 // Owner: Colton Nunley
 // Contributers: Joseph Remy, Julian Bell, and Tanner Massahos
 
+// Required imports
+#include <stdio.h>
+#include <stdlib.h>
+
+// Method pre-declarations
+int characterToInt(char* charArray);
+int characteristic(char numString[], int* c);
+int mantissa(char numString[], int* numerator, int* denominator);
+
 char number[] = "123.456";
 int c, n, d;
 
+// Converts a char to an integer
+int characterToInt(char* charArray)
+{
+  int result = 0;
+  for (int index = 0; charArray[index] != '\0'; index++)
+  {
+    result = (result*10) + (charArray[index] - '0');
+  }
+  return result;
+}
+
 // Returns the part of a float before the decimal (like a floor function)
-bool characteristic(char numString[], int& c)
+int characteristic(char numString[], int* c)
 {
 	printf("%s\n", numString);
+	return 0;
 }
 
 // Returns the part of a float after decimal (significand)
-bool mantissa(char numString[], int& numerator, int& denominator)
+int mantissa(char numString[], int* numerator, int* denominator)
 {
 	printf("%s\n", numString);
+	return 0;
 }
 
-
-
- 
-//if the conversion from C string to integers can take place
-if(characteristic(number, c) && mantissa(number, n, d))
+// If the conversion from C string to integers can take place
+int main()
 {
-    //do some math with c, n, and d
-}
-else
-{
-    //handle the error on input
+	if(characteristic(number, &c) && mantissa(number, &n, &d))
+	{
+	    //do some math with c, n, and d
+	}
+	else
+	{
+	    //handle the error on input
+	}
+	return 1;
 }
